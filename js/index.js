411ad7dfdb2319af2ed6b4afb994c98ddc1902f4
Alexander <alexander.davidson87@gmail.com>
2022-05-17 13:55:06 -0500

    Collapsible Event
    Created collapsible event section, working on script still

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
-// Initializes and adds the map
-function initMap() {
-  // Location of Iron Tusk Art
-  const ironTuskArt = { lat: 35.68, lng: -85.77 };
-  // Centers map on Iron Tusk Art
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: ironTuskArt,
-  });
-  // The marker on the map
-  const marker = new google.maps.Marker({
-    position: ironTuskArt,
-    map: map
-  });
-}
+// // Initializes and adds the map
+// function initMap() {
+//   // Location of Iron Tusk Art
+//   const ironTuskArt = { lat: 35.68, lng: -85.77 };
+//   // Centers map on Iron Tusk Art
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: ironTuskArt,
+//   });
+//   // The marker on the map
+//   const marker = new google.maps.Marker({
+//     position: ironTuskArt,
+//     map: map
+//   });
+// }
+// window.initMap = initMap;
 
 function navbarCollapse() {
   var x = document.getElementById("mainNavbar");
@@ -23,4 +24,19 @@ function navbarCollapse() {
   }
 }
 
-window.initMap = initMap;
+
+var collapse = document.getElementsById("collapsible");
+var i;
+
+for (i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
