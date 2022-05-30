464e583fb6078c187d57f2e0b7b9ffd8515b615f
Alexander <alexander.davidson87@gmail.com>
2022-05-30 15:09:01 -0500

    contact section reworked
    contact section reworked, styling updates, added updated header and footer to all pages

---
+++ js/index.js
@@ -0,0 +1,24 @@
+(function () {
+  emailjs.init("nUqeD7kqFEtTTlzg5");
+})();
+
+document
+      .getElementById("contact-form")
+      .addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const serviceID = "service_ef0excg";
+        const templateID = "template_qll60y8";
+
+        // send the email here
+        emailjs.sendForm(serviceID, templateID, this).then(
+          (response) => {
+            console.log("SUCCESS!", response.status, response.text);
+            alert("SUCCESS!");
+          },
+          (error) => {
+            console.log("FAILED...", error);
+            alert("FAILED...", error);
+          }
+        );
+      });
