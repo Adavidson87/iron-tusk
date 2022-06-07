0f265d025b79f65a818376736d2ae65a24752ea7
Alexander <alexander.davidson87@gmail.com>
2022-06-07 15:47:47 -0500

    merch contact faq
    updates and worked on merch, contact, and faq pages.

--- js/index.js
+++ js/index.js
@@ -3,22 +3,22 @@
 })();
 
 document
-      .getElementById("contact-form")
-      .addEventListener("submit", function (event) {
-        event.preventDefault();
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        const serviceID = "service_ef0excg";
-        const templateID = "template_qll60y8";
+    const serviceID = "service_ef0excg";
+    const templateID = "template_qll60y8";
 
-        // send the email here
-        emailjs.sendForm(serviceID, templateID, this).then(
-          (response) => {
-            console.log("SUCCESS!", response.status, response.text);
-            alert("SUCCESS!");
-          },
-          (error) => {
-            console.log("FAILED...", error);
-            alert("FAILED...", error);
-          }
-        );
-      });
+    // send the email here
+    emailjs.sendForm(serviceID, templateID, this).then(
+      (response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("SUCCESS!");
+      },
+      (error) => {
+        console.log("FAILED...", error);
+        alert("FAILED...", error);
+      }
+    );
+  });
